7a6b0c410a30ea36658ea34455e5bc45fe02ff14
Daisy Maldonado <madaisy677@gmail.com>
2025-09-25 16:19:55 -0500

    test implementation

--- src/test/java/HMTest.java
+++ src/test/java/HMTest.java
@@ -3,28 +3,27 @@ import org.junit.jupiter.api.Test;
 
 public class HMTest {
 	
+	// adds the given key-value pair into map correctly 
 	@Test
 	void ConstructorTest() {
         MyHashMap<Integer> map = new MyHashMap<>("key1", 100);
 		assertEquals(100,map.get("key1"), "The value at key1 should be 100");
 	}
 	
+	// check if put function ????
 	@Test
 	void putTest() {
 		MyHashMap<Integer> map = new MyHashMap<>("key1", 100);
         map.put("key2", 200);
         
+        // check keys
+		assertTrue(map.contains("key1"), "Map should contain key1");
+	    assertTrue(map.contains("key2"), "Map should contain key2");
+	    assertFalse(map.contains("key3"), "Map should not contain key3");
+        
+        // check values
         assertEquals(100, map.get("key1"), "The value at key1 should be 100");
 		assertEquals(200, map.get("key2"), "The value at key2 should be 200");
-		
-//		assertEquals(200, map.get("key2"), "The value at key2 should be 200");
-//        
-//        map.put("key3", 300);
-
-//		
-//		assertEquals(300,map.get("key3"), "The value at key3 should be 300");
-//		assertEquals(null,map.get("key4"), "The value at key4 doesn't exist");
-		
 	}
 	
 	@Test
